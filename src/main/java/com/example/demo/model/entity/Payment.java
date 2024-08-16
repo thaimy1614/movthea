6fff8830adfc57d252c207df6b6abba507c3aaed
quocthai <lxlthailxl@gmail.com>
2024-08-16 17:14:23 +0700

    init payment entity

--- src/main/java/com/example/demo/model/entity/Payment.java
+++ src/main/java/com/example/demo/model/entity/Payment.java
@@ -1,4 +1,29 @@
 package com.example.demo.model.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ticket_id")
+    private Ticket ticket;
+
+    @Column(name = "payment_date")
+    private LocalDate paymentDate;
+
+    private Long price;
 }
